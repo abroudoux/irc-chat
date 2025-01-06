2c54e727c62a0c3f5b1bcca3449ab44dc18daf8f
arthur <arthur.broudoux@gmail.com>
2025-01-06 11:57:46 +0100

    feat: create socket.io connection, send message on frontend from react application Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+
 export default function App() {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  function sendMessage() {
+    socket.emit("send_message", { message: message });
+  }
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <div className="App">
+      <input
+        placeholder="Message"
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+      />
+      <button onClick={sendMessage}>Send message</button>
+      <h1>Message: {messageReceived}</h1>
+    </div>
   );
 }
--- server/main.mjs
+++ server/main.mjs
@@ -1,12 +1,31 @@
 import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
+const PORT = 3000;
 const app = express();
-const port = 3000;
+app.use(cors());
 
-app.get("/", (_, res) => {
-  res.send("Hello, World!");
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5174"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  },
+  debug: true,
 });
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
+io.on("connection", (socket) => {
+  console.log(`a user connected ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("receive_message", data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
 });
