0e04d6fd163a36c618ab0292d8a0d2b7c8d7a34f
arthur <arthur.broudoux@gmail.com>
2025-01-13 10:18:39 +0100

    feat: update Auth page Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,7 +1,16 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 export default function Auth() {
   return (
-    <div>
-      <h1>Auth</h1>
-    </div>
+    <section className="w-screen h-screen flex flex-col justify-center items-center gap-8">
+      <h1 className="text-4xl font-semibold">
+        Choose a pseudo to start chatting
+      </h1>
+      <div className="flex flex-row gap-2 items-center">
+        <Input type="text" placeholder="Your pseudo" className="max-w-lg" />
+        <Button>Start chatting</Button>
+      </div>
+    </section>
   );
 }
