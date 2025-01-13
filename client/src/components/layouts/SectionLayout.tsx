046053a12cedb65eeeea04aad58416e3006d31dd
arthur <arthur.broudoux@gmail.com>
2025-01-13 11:42:25 +0100

    chore: minor changes Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- client/src/components/layouts/SectionLayout.tsx
+++ client/src/components/layouts/SectionLayout.tsx
@@ -3,6 +3,8 @@ import { SectionLayoutProps } from "@/utils/interfaces";
 
 export default function SectionLayout(props: SectionLayoutProps) {
   return (
-    <section className={cn("p-4", props.className)}>{props.children}</section>
+    <section className={cn("p-4 overflow-hidden", props.className)}>
+      {props.children}
+    </section>
   );
 }
